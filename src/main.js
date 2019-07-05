cd47606085002209a8c7bf3f2facf14601a30658
Josh <Joshua.horgen@gmail.com>
2019-07-05 13:45:17 -0700

    Update js, main, and index files with a second promise/function

--- src/main.js
+++ src/main.js
@@ -1,27 +1,33 @@
-import { DoctorNotes } from "./doctor-info.js";
+import { DoctorInfo } from "./doctor.js";
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+let doctorNotes = new DoctorInfo();
 
 $(document).ready(function(){
   $(".inputForm").submit(function(event){
     event.preventDefault();
 
-    let newInput = $('#search').val();
-    console.log(newInput);
+    let nameInput = $('#nameInput').val();
+    let medicalInput = $('#medicalInput').val();
+    console.log(medicalInput);
 
-    let doctorNotes = new DoctorNotes();
-    let promise = doctorNotes.generateNotes(newInput);
+    let namePromise = doctorNotes.searchByName(nameInput);
+    namePromise.then(function(response) {
+      let body = JSON.parse(response).data;
+      console.log(body);
+      $('.output').text(body);
+    });
 
+      let medicalPromise = doctorNotes.searchByMedical(medicalInput);
+      medicalPromise.then(function(response){
+        let body = JSON.parse(response).data;
+        console.log(body);
 
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      var keys = Object.values(body.meta);
-      console.log(keys);
-      $('.output').text(keys);
+      });
+
 
     });
   });
-});
