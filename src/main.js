5ceda8a2a1f40cb4989586baf3cb2d21b8513e12
Josh <Joshua.horgen@gmail.com>
2019-07-05 09:31:26 -0700

    Update main.JS, index, etc

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// import {  } from "./doctor-js.js";
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+$(document).ready(function(){
+  $(".inputForm").submit(function(event){
+    event.preventDefault();
+
+const newInput = $('#search').val();
+console.log(newInput);
+
+
+  });
+});
