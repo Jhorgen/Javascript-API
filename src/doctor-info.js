c94581d6e035c4a94f1884fe374b510107ded9f7
Josh <Joshua.horgen@gmail.com>
2019-07-05 11:56:30 -0700

    Experimenting with api

--- src/doctor-info.js
+++ src/doctor-info.js
@@ -1,10 +1,9 @@
-
 export class DoctorNotes {
-  generateNotes() {
-    console.log(generateNotes);
+  generateNotes(newInput) {
+    // console.log(generateNotes);
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=1cc9d6cd76cce8d4188e897e5744d7dc`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=1cc9d6cd76cce8d4188e897e5744d7dc`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -17,8 +16,3 @@ export class DoctorNotes {
     });
   }
 }
-
-
-
-
-https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=1cc9d6cd76cce8d4188e897e5744d7dc
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { DoctorNotes } from "./doctor-js.js";
+import { DoctorNotes } from "./doctor-info.js";
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,9 +9,18 @@ $(document).ready(function(){
   $(".inputForm").submit(function(event){
     event.preventDefault();
 
-const newInput = $('#search').val();
-console.log(newInput);
+    let newInput = $('#search').val();
+    console.log(newInput);
+
+    let doctorNotes = new DoctorNotes();
+    let promise = doctorNotes.generateNotes(newInput);
 
 
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      console.log(body);
+      $('.output').text(body);
+
+    });
   });
 });
