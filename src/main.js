9618595925120b7ea7969b029ce34ea7bfbef5e7
Josh <Joshua.horgen@gmail.com>
2019-07-10 20:57:33 -0700

    Refactor and correct type errors

--- src/main.js
+++ src/main.js
@@ -4,45 +4,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-let doctorNotes = new DoctorInfo();
 
 $(document).ready(function(){
-  $('.nameForm').hide();
-  $('.medForm').hide();
-  $('.selectForm').change(function() {
-    if ($(this).val() === 'name') {
-      $('.nameForm').show();
-      $('.medForm').hide();
-    } else if ($(this).val() === 'medical') {
-      $('.medForm').show();
-      $('.nameForm').hide();
-    }
-  });
-
-  $(".nameForm").submit(function(event){
+  let doctorNotes = new DoctorInfo();
+  $(".inputForm").submit(function(event){
     event.preventDefault();
-
     let nameInput = $('#nameInput').val();
-    console.log(medicalInput);
+    let symptom = $('#symptom').val();
 
-    let namePromise = doctorNotes.searchByName(nameInput);
-    namePromise.then(function(response) {
+    let Promise = doctorNotes.searchForDoctors(nameInput, symptom);
+    Promise.then(function(response){
       let body = JSON.parse(response).data;
-      console.log(body);
-      $('.output').text(body);
-    });
-  });
 
-  let medicalInput = $('#medicalInput').val();
-  $(".medForm").submit(function(event){
-    event.preventDefault();
-    let medicalPromise = doctorNotes.searchByMedical(medicalInput);
-    medicalPromise.then(function(response){
-      let body = JSON.parse(response);
-      console.log(body.data);
+      body.forEach(function(searchThis){
+        console.log(body);
+        $('.output').append(`<li> ${searchThis.profile.bio}</li>`);
 
-      $('.output').text(JSON.stringify(body));
-
+      });
     });
   });
 });
