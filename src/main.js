2bdb44bf0b082576d668ec98424f0efe67013afc
Josh <Joshua.horgen@gmail.com>
2019-07-14 18:11:45 -0700

    Update for each loop

--- src/main.js
+++ src/main.js
@@ -16,11 +16,21 @@ $(document).ready(function(){
       let body = JSON.parse(response).data;
       console.log(body);
       if (body.length === 0 ) {
-        $('.output').html("Please search again");
+        $('.output').html("Invalid entry, please search again");
       } else {
         $('.output').html("Your results are:");
         body.forEach(function(searchThis){
-          $('.output').append(`<li> ${searchThis.profile.bio}</li>`);
+
+          let nameSearch = searchThis.profile.first_name + ' ' + searchThis.profile.last_name + '<br>' + 'Name of practice:'  + ' ' + searchThis.practices[0].name;
+
+          let symptomSearch = searchThis.profile.title + "<br>" + searchThis.specialties[0].description;
+
+
+          let docAddress = searchThis.practices[0].visit_address.street + ' ' +
+          searchThis.practices[0].visit_address.zip;
+
+          $('.output').append(`<li> ${nameSearch} ${symptomSearch} <br> Address: ${docAddress}</li> <br>`);
+
           });
       }
     });
